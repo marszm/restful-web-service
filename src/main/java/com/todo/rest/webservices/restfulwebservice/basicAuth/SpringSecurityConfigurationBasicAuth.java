2b386859570129e2180c77c784301a0a6a53f87d
marszm <mariusz.szmer@gmail.com>
2019-09-15 14:51:59 +0200

    add spring security configuration class

---
+++ src/main/java/com/todo/rest/webservices/restfulwebservice/basicAuth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,28 @@
+package com.todo.rest.webservices.restfulwebservice.basicAuth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+//                .formLogin()
+//                .and()
+                .httpBasic();
+    }
+
+
+}
